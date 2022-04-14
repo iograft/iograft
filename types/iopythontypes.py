6f860fa87394c8fb607d30faf0fb66fe9d46ade5
Dan Knowlton <dan@iograft.com>
2022-04-14 12:53:50 -0700

    Update iopythontypes plugin based on version 1.0.0 API changes

--- types/iopythontypes.py
+++ types/iopythontypes.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Fabrica Software, LLC
+# Copyright 2022 Fabrica Software, LLC
 
 import iograft
 
@@ -18,7 +18,7 @@ class DictList(iograft.PythonListType):
     Type representing a list of Python dict objects.
     """
     def __init__(self):
-        super(DictList, self).__init__(Dict().GetTypeId())
+        super(DictList, self).__init__(Dict.type_id)
 
 
 class Tuple(iograft.PythonType):
@@ -32,25 +32,30 @@ class Tuple(iograft.PythonType):
 
 
 class TupleList(iograft.PythonListType):
+    """
+    Type representing a list of Python tuple objects.
+    """
     def __init__(self):
-        super(Tuple, self).__init__(Tuple().GetTypeId())
+        super(TupleList, self).__init__(Tuple.type_id)
 
 
 def LoadPlugin(plugin):
     # Register the dict type with iograft.
     dict_type = plugin.RegisterPythonType(Dict.type_id,
+                                          Dict(),
                                           iograft.ValueToString,
                                           iograft.ValueFromString,
                                           iograft.SerializeValue,
                                           iograft.DeserializeValue)
     # Also register the list type for dicts.
-    plugin.RegisterPythonListType(dict_type)
+    plugin.RegisterPythonListType(dict_type, DictList())
 
     # Register the tuple type with iograft.
     tuple_type = plugin.RegisterPythonType(Tuple.type_id,
+                                           Tuple(),
                                            iograft.ValueToString,
                                            iograft.ValueFromString,
                                            iograft.SerializeValue,
                                            iograft.DeserializeValue)
     # Also register the list type for tuples.
-    plugin.RegisterPythonListType(tuple_type)
+    plugin.RegisterPythonListType(tuple_type, TupleList())
